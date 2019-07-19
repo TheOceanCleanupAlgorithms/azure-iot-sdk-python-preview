006679dc1d85fb0bad71c125078abedbfbb540fe
Carter Tinney <catinney@microsoft.com>
2019-07-18 17:30:16 -0700

    removed lower level edge/http pipeline modules (for now)

--- azure-iot-device/azure/iot/device/iothub/pipeline/edge_pipeline.py
+++ azure-iot-device/azure/iot/device/iothub/pipeline/edge_pipeline.py
@@ -5,8 +5,6 @@
 # --------------------------------------------------------------------------
 
 import logging
-from azure.iot.device.common.pipeline import pipeline_stages_base, pipeline_stages_http
-from . import pipeline_ops_edge, pipeline_stages_edge, pipeline_stages_edge_http
 
 logger = logging.getLogger(__name__)
 
@@ -17,27 +15,4 @@ class EdgePipeline(object):
     """
 
     def __init__(self, auth_provider):
-        self._pipeline = (
-            pipeline_stages_base.PipelineRootStage()
-            .append_stage(pipeline_stages_edge.UseAuthProviderStage())
-            .append_stage(pipeline_stages_edge_http.EdgeHTTPConverterStage())
-            .append_stage(pipeline_stages_http.HTTPTransportStage())
-        )
-
-        # TODO: something really needs to be done about this
-        # It's been hanging around for far too long
-        def remove_this_code(call):
-            if call.error:
-                raise call.error
-
-        self._pipeline.run_op(
-            pipeline_ops_edge.SetAuthProviderOperation(
-                auth_provider=auth_provider, callback=remove_this_code
-            )
-        )
-
-    def invoke_device_method(self):
-        pass
-
-    def invoke_module_method(self):
         pass
